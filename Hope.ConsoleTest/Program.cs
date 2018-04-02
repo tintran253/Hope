c0f5143bddb7e0b4e3c78b549d1c8285048a8102
Tin'sHome <tintran2008@gmail.com>
2018-04-02 18:40:42 +0700

    missing files

---
+++ Hope.ConsoleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hope.ConsoleTest
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "id_1", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api/composers");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/composers");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
---
+++ Hope.IS4/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Hope.IS4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddIdentityServer()
+                    .AddDeveloperSigningCredential()
+                    .AddInMemoryClients(new List<Client> {
+                        new Client{
+                            ClientId = "id_1" ,
+                            AllowedGrantTypes = GrantTypes.ClientCredentials,
+                            ClientSecrets = {new Secret("secret".Sha256())},
+                            AllowedScopes = { "api/composers" }
+
+                        },
+                        new Client{
+                            ClientId = "id_2" ,
+                            AllowedGrantTypes = GrantTypes.ClientCredentials,
+                            ClientSecrets = {new Secret("secret".Sha256())},
+                            AllowedScopes = { "api/composers" }
+                        }
+                    })
+                    .AddInMemoryIdentityResources(new List<IdentityResource>
+                    {
+                        new IdentityResource { Name = "A" },
+                        new IdentityResource { Name = "B" },
+                    })
+                    .AddInMemoryApiResources(new List<ApiResource>
+                    {
+                        new ApiResource("api/composers", "APITTT")
+                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseIdentityServer();
+
+            app.UseMvc();
+        }
+    }
+}
